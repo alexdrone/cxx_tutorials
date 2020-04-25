e3109bea6efb9a27e566d70ecab18efa726c91e0
Alex Usbergo <alexusbergo@alexusbergo-imacpro.roam.corp.google.com>
2020-04-25 22:18:33 +0200

    abstracting 2

---
+++ opengl-template/opengl-template/vertex_array.cxx
@@ -0,0 +1,9 @@
+//
+//  vertex_array.cpp
+//  opengl-template
+//
+//  Created by Alex Usbergo on 2020-04-25.
+//  Copyright © 2020 Alex Usbergo. All rights reserved.
+//
+
+#include "vertex_array.hpp"
---
+++ opengl-template/opengl-template/vertex_array.h
@@ -0,0 +1,14 @@
+//
+//  vertex_array.hpp
+//  opengl-template
+//
+//  Created by Alex Usbergo on 2020-04-25.
+//  Copyright © 2020 Alex Usbergo. All rights reserved.
+//
+
+#ifndef vertex_array_hpp
+#define vertex_array_hpp
+
+#include <stdio.h>
+
+#endif /* vertex_array_hpp */
